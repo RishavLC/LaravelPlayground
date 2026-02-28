37d549868d58a2b57d2ed7f09a329e4b964bf0d2
RishavLC <shrestharishav444@gmail.com>
2026-02-28 21:54:49 +0545

    made a form with routes and controller

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,20 @@ Route::get('/index', [UserController::class, 'index']);
 Route::get('/about', function(){
     return "This is about page";
 });
+
+Route::get('/form', function () {
+    return '
+        <form method="POST" action="/submit">
+            <input type="text" name="name" placeholder="Enter your name"
+            style="padding: 10px; border: 1px solid #ccc; border-radius: 40px; width: 200px;">
+            <input type="email" name="email" placeholder="Enter your mail address" 
+            style="border: 1px solid #ccc; border-radius: 40px; width: 200px; padding: 10px;">
+            <input type="password" name="password" placeholder="Enter your password"
+            style="border: 1px solid #ccc; border-radius: 40px; width: 200px; padding: 10px;">
+            <input type="number" name="age" placeholder="Enter your age"
+            style="border: 1px solid #ccc; border-radius: 40px; width: 200px; padding: 10px;">
+            <button type="submit"
+            style="padding: 10px 20px; background-color: #2e0b0b; color: white; border: none; border-radius: 40px; cursor: pointer;">Submit</button>
+        </form>
+    ';
+});
