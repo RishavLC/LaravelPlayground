c4093b3f1e896fb8b4cfd4931a642928fefe6019
RishavLC <shrestharishav444@gmail.com>
2026-04-22 23:52:25 +0545

    ading css

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,6 +15,7 @@ class ProductController extends Controller
     public function create()
     {
         return view('products.create');
+        return view('products.create');
     }
 
     // STORE
